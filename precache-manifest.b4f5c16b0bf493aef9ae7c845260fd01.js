e900be1f8afa2fab540d441e192e35aabeec73fa
ravi <ravi8688@gmail.com>
2022-07-20 12:18:25 +0530

    Updates

---
+++ precache-manifest.b4f5c16b0bf493aef9ae7c845260fd01.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5fa5f16e93692efa279dc15735d5c53",
+    "url": "/weaather-repot/index.html"
+  },
+  {
+    "revision": "3f84412c40f19f9d098c",
+    "url": "/weaather-repot/static/css/main.ad7a34fc.chunk.css"
+  },
+  {
+    "revision": "44d91217a2106cc453b8",
+    "url": "/weaather-repot/static/js/2.6fc8ad99.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weaather-repot/static/js/2.6fc8ad99.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3f84412c40f19f9d098c",
+    "url": "/weaather-repot/static/js/main.36f16e8a.chunk.js"
+  },
+  {
+    "revision": "2bdf627ed2ca6edb6b5b",
+    "url": "/weaather-repot/static/js/runtime-main.ce4eb3db.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/weaather-repot/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/weaather-repot/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
